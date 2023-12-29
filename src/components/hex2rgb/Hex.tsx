771f8f62e35ed5728bbe27f30e923ffb28fba79e
Yury Vereskun <satriks@mail.ru>
2023-12-29 15:05:08 +0300

    fix var

--- src/components/hex2rgb/Hex.tsx
+++ src/components/hex2rgb/Hex.tsx
@@ -23,7 +23,7 @@ const chooseColor = (event: React.FormEvent) => {
 }   
 
 function hexToRgb(hex : string) {
-    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? `rgb: (${parseInt(result[1], 16)},  ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})` : null;
   }
 
